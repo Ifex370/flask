44d43d764b2a3be6aac265e32c423e439093cc9c
st6 <kemkeut@gmail.com>
2022-02-05 21:53:50 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# LS3
+# ls3 simple flask app
+
+A very simple Flask app that displays date and time according to the TimeZone rendering on html template.
+
+- $ pip install -r requirements.txt
+- $ python app.py
+
+That's all 😻!
+
+The page refreshes automatically every minute!
+
+You can also deploy the Docker version available here!!!!
+
+
---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask import render_template
+import time
+
+app=Flask(__name__)
+
+@app.route('/')
+def current_day_and_time():
+	time_now=time.strftime('%I:%M %p %Z')
+	day_today=time.strftime('%A, %B %d, %Y')
+	
+	return render_template('time.html',
+	    time_now=time_now,
+	    day_today=day_today
+	    )
+
+if __name__=='__main__':
+	app.run(debug=True, port=8000)
