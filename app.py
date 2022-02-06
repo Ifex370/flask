6c728e5c9a0ad6fa2497bdd9f9e66364ad09979a
st6 <kemkeut@gmail.com>
2022-02-06 04:13:49 +0300

    Initial commit

--- app.py
+++ app.py
@@ -14,5 +14,18 @@ def current_day_and_time():
 	    day_today=day_today
 	    )
 
+@app.route('/blog')
+def hello_blog():
+    return ("<h1>Empty Blog lol (^_^)</h1><p>Nothing to see now!</p> ")
+	    
+
+@app.route('/gallery')
+def hello_gallery():
+    return ("<h1>Empty Gallery lol (*_*)</h1><p>Nothing to see now!</p> ")
+
+@app.route('/memes')
+def hello_memes():
+    return ("<h1>Empty lol (0__0)</h1><p>Nothing to see now!</p> ")
+
 if __name__=='__main__':
 	app.run(debug=True)
