37d90035fefb39f6784cda211d1c158fe9c8b221
ifeanyi <ifex370@gmail.com>
2022-02-17 08:15:02 -0500

    Large System 3

--- app/app.py
+++ app/app.py
@@ -9,9 +9,7 @@ def current_day_and_time():
 	time_now=time.strftime('%I:%M %p %Z')
 	day_today=time.strftime('%A, %B %d, %Y')
 	
-	return render_template('time.html',
-	    time_now=time_now,
-	    day_today=day_today
+	return render_template('index.html',
 	    )
 
 @app.route('/blog')
